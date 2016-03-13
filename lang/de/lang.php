be850bf1a05ee0b0747af2abee78e08d53088837
rmdev <rmdev@rawman.de>
2016-03-13 12:36:10 +0100

    german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'strings'     => [
+        'plugin_desc'               => 'Ein flexibles und konfigurierbares Kontaktformular',
+        'email_desc'                => 'Email die dem Administrator gesendet wird, nachdem das Formular ausgefüllt wurde.',
+        'recipient_email'           => 'Empfänger Email',
+        'recipient_email_comment'   => 'Die Person zu der die Email versandt wird',
+        'recipient_email_desc'      => 'Die Person zu der die Email versandt wird',
+        'recipient_name'            => 'Empfänger Name',
+        'recipient_name_comment'    => 'Der kopmlette Name an den die Email versandt wird',
+        'recipient_name_desc'       => 'Der kopmlette Name an den die Email versandt wird',
+        'subject'                   => 'Betreff',
+        'subject_comment'           => 'Der Bretreff der Email die Sie erhalten',
+        'subject_desc'              => 'Der Bretreff der Email die Sie erhalten',
+        'confirmation_text'         => 'Bestätigungstext',
+        'confirmation_text_comment' => 'Der Bestätigungstext nach absenden des Formulars',
+        'confirmation_text_desc'    => 'Der Bestätigungstext nach absenden des Formulars',
+        'settings_label'            => 'Flexi Contact Einstellungen',
+        'settings_desc'             => 'Einstellungen des Flexi Kontaktformulars',
+        'component_name'            => 'Kontaktformular',
+        'component_desc'            => 'Zeigt das Kontaktformular',
+        'inject_bootstrap'          => 'Bootstrap Assets einbinden',
+        'inject_bootstrap_desc'     => 'Sollen Bootstrap css and javascript Dateien eingebunden werden',
+        'inject_main_script'        => 'Haupt JavaScript einbinden',
+        'inject_main_script_desc'   => 'Das Hauptscript dieser Komponente einbinden',
+        'site_key'                  => 'Seiten Schlüssel (Site Key)',
+        'site_key_comment'          => 'Der Site Key den Sie bei der Einrichtung von Google bekommen haben',
+        'site_key_desc'             => 'Der Site Key den Sie bei der Einrichtung von Google bekommen haben',
+        'secret_key'                => 'Geheimer Schlüssel (Secret Key)',
+        'secret_key_comment'        => 'Der geheime Schlüssel den Sie bei der Einrichtung von Google bekommen haben',
+        'secret_key_desc'           => 'Der geheime Schlüssel den Sie bei der Einrichtung von Google bekommen haben',
+        'enable_server_captcha_validation'            => 'Server Captcha Validierung aktivieren',
+        'enable_server_captcha_validation_comment'    => 'Sollen die captcha credentials zum validieren zu Google gesendet werden (optional aber empfohlen)',
+        'enable_server_captcha_validation_desc'       => 'Sollen die captcha credentials zum validieren zu Google gesendet werden (optional aber empfohlen)'
+    ],
+    'permissions' => [
+        'tab'      => 'Flexi Contact',
+        'settings' => 'Zugriffseinstellungen',
+    ],
+];
